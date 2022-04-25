cc71f7eb68238f4ddb43b19541f4d99276c9e993
Oyedunmade <oyesalawu1@gmail.com>
2022-04-24 17:43:16 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,31 @@
+
+holladortun
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+alx-low_level_programming/0x06-pointers_arrays_strings/main.h
+@holladortun
+holladortun tasks commit
+ 1 contributor
+13 lines (13 sloc)  440 Bytes
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char);
