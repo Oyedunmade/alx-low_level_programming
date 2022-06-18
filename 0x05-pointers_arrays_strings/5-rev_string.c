92f2b0212fa5e3639c9a0b4cea533acb3ba74e58
Oyedunmade <oyesalawu1@gmail.com>
2022-06-17 18:31:03 -0700

    Commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *rev_string - reverses a string.
- *@s: input string.
- *Return: no return.
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
  */
 void rev_string(char *s)
 {
@@ -27,3 +27,4 @@ void rev_string(char *s)
 			*(str + (j - 1)) = temp;
 		}
 	}
+}
